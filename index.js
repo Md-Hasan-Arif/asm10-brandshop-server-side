79dcb8720970c0406e37d5fe674e16b5ff7fc303
Hasan Arif <mdhasanarifh@gmail.com>
2023-10-20 01:17:23 +0600

    added delete oparation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.MDB_USER}:${process.env.MDB_PASS}@cluster0.5tb2p8r.mongodb.net/?retryWrites=true&w=majority`;
-
+// git remote add origin https://github.com/programming-hero-web-course-4/b8a10-brandshop-server-side-Md-Hasan-Arif.git
 
 
 
@@ -27,12 +27,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('productDB').collection('product')
+
+    app.get('/addProduct',async (req,res)=>{
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/addProduct',async(req, res)=>{
+      const newProduct= req.body;
+      console.log(newProduct)
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    app.delete('/addProduct/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -41,6 +63,8 @@ run().catch(console.dir);
 app.get('/', (req,res)=>{
     res.send('Hey Server is running')
 })
+
+
 
 app.listen(port,()=>{
     console.log(`Your Server is running on port:${port}`)
